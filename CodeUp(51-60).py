1e034ae2dff8c2a4ffa78f90c8f667c0f4657041
unknown <seoara0109@gmail.com>
2022-10-03 21:10:35 +0900

    Solve probs #51~60

---
+++ CodeUp(51-60).py
@@ -0,0 +1,39 @@
+#6051
+a, b = input().split()
+print(int(a)!=int(b))
+
+#6052
+n = int(input())
+print(bool(n))
+
+#6053
+a = bool(int(input()))
+print(not a)
+
+#6054
+a, b = input().split()
+print(bool(int(a)) and bool(int(b)))
+
+#6055
+a, b = input().split()
+print(bool(int(a)) or bool(int(b)))
+
+#6056
+a, b = input().split()
+print(bool(int(a)) != bool(int(b)))
+
+#6057
+a, b = input().split()
+print(bool(int(a)) == bool(int(b)))
+
+#6058
+a, b = input().split()
+print(bool(int(a)) == False and bool(int(b)) == False)
+
+#6059
+a = int(input())
+print(~a)
+
+#6060
+a, b = input().split()
+print(int(a)&int(b))
